a9d0e00d2e76ea2230ed6809233013608a8631ef
PoohL0ve <screwed06@gmail.com>
2023-04-17 02:16:33 -0400

    while loop set to true asking the user yes/no

---
+++ mooc-fi-python/loops/while/shall-we-continue/shallwe.py
@@ -0,0 +1,8 @@
+# if not is inputted by the user, end the program
+
+while True :
+    greeting = print('hi')
+    you_said = input('Shall we contine: ')
+    if you_said == 'no' :
+        break
+print('okay then')
