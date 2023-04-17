88769ed697aa59eb7c4c0135cb1657f02fa54169
PoohL0ve <screwed06@gmail.com>
2023-04-17 02:43:06 -0400

    Fixed countdown

---
+++ mooc-fi-python/loops/while/countdown/fixcount.py
@@ -0,0 +1,22 @@
+# fix the code to print 5 4 3 2 1
+#number = 5
+#print("Countdown!")
+#while True:
+  #print(number)
+  #number = number - 1
+  #if number > 0:
+    #break
+
+#print("Now!")
+
+# Above is the old program
+
+number = 5
+print('Countdown!')
+
+while True :
+    print(number)
+    number = number - 1
+    if number <= 0 :
+        break
+print('Now')
