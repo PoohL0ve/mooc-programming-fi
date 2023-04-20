15b6bababe7c91774c2014c8fa29a489763cd0ce
PoohL0ve <screwed06@gmail.com>
2023-04-20 00:15:11 -0400

    Adjusted code that allows the while lope to function correctly

---
+++ mooc-fi-python/loops/while/countdown/fixcount2.py
@@ -0,0 +1,15 @@
+# Code that needs to be adjusted:
+    #print("Are you ready?")
+    #number = int(input("Please type in a number: "))
+    #while number = 0:
+    #print(number)
+    #print("Now!")
+# End of error prone code
+
+print('Are you ready?')
+number = int(input('Please enter a number: '))
+
+while number > 0 :
+    print(number)
+    number -= 1
+print('Now')
