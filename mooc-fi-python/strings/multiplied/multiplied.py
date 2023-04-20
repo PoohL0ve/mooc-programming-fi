97feb0b71874380dd200574badf1c61d68cb2f80
PoohL0ve <screwed06@gmail.com>
2023-04-20 04:30:26 -0400

    Strings and the * operator

---
+++ mooc-fi-python/strings/multiplied/multiplied.py
@@ -0,0 +1,7 @@
+# ask user for a string and an integer
+# print the string the number of times the integer specified: no spaces
+
+thing = input('Enter something: ')
+number = int(input('Enter an integer: '))
+
+print(thing*number)
