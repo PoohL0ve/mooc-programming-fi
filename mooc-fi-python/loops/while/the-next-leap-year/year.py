78aac7beb72f269d3660234ef6f2ed4843650581
PoohL0ve <screwed06@gmail.com>
2023-04-21 06:18:04 -0400

    Leap Year Checker

--- mooc-fi-python/loops/while/the-next-leap-year/year.py
+++ mooc-fi-python/loops/while/the-next-leap-year/year.py
@@ -1,17 +1,21 @@
 # print the following leap year
+this_year = int(input('Enter a year: '))
 leap_year = 0
 while True :
-    this_year = int(input('Enter the year: '))
-
-    if this_year % 3 == 0 :
-        leap_year = this_year + 1
-        break
-    print('Year: ', this_year)
-    print('The next leap year is: ', leap_year)
-
-    if this_year % 2 == 0 :
-        leap_year = this_year + 4
-        break
-    print('Year: ', this_year)
-    print('The next leap year is ', leap_year)
-print('Yay, your calender is updated')
+   if this_year % 4 == 0 and this_year % 100 != 0 :
+    leap_year = this_year + 4 
+    break
+   
+   if this_year % 4 == 1 :
+    leap_year = this_year + 3
+    break
+   
+   if this_year % 4 == 2 :
+    leap_year = this_year + 2
+    break
+   
+   if this_year % 4 == 3 :
+    leap_year = this_year + 1
+    break
+   
+print('The next leap year is: ', leap_year) 
