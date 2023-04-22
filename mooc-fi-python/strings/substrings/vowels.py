ce91331c10bbafd345f9a9a918b533cc5fdc6fc7
PoohL0ve <screwed06@gmail.com>
2023-04-21 22:50:48 -0400

    Finding vowels in substrings using the in operator

---
+++ mooc-fi-python/strings/substrings/vowels.py
@@ -0,0 +1,12 @@
+# searching for a, e, o from user input
+
+statement = str(input('Tell me something: '))
+
+if 'a' in statement :
+    print('a is found')
+if 'e' in statement :
+    print('e is found')
+if 'o' in statement :
+    print('o is found')
+else :
+    print('Searched vowels not found')
