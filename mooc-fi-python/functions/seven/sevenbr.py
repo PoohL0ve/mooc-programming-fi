90e32c279e5fa10ceed071bb80f0ebdd74f212c4
PoohL0ve <screwed06@gmail.com>
2023-04-24 04:23:23 -0400

    Prints the name of seven brothers when the program is called

---
+++ mooc-fi-python/functions/seven/sevenbr.py
@@ -0,0 +1,12 @@
+# function that prints out seven brothers when called
+
+def seven_brothers() :
+    print('Alvin')
+    print('Theodore')
+    print('Josh')
+    print('Callum')
+    print('Hakim')
+    print('Bart')
+    print('Keely')
+
+seven_brothers()
