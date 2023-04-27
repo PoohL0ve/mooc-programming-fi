6be1527d9d992e9e5a10764f12b96411b89dae1f
PoohL0ve <screwed06@gmail.com>
2023-04-27 00:46:11 -0400

    Displaying characters in a string using a for loop

---
+++ mooc-fi-python/loops/for/characters/star_studded.py
@@ -0,0 +1,9 @@
+# print each character in a string on a separate line
+# after each line print a star
+# use a for lop (definite iteration)
+
+word = input('Enter a word: ')
+
+for character in word :
+    print(character)
+    print('*')
